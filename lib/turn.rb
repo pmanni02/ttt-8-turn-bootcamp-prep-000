3988c7d148f344781bf18b3b74ca54ce2a2fb8eb
Phoebe Manning <phoebe.manning8@gmail.com>
2017-09-25 17:12:01 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,7 +4,7 @@ def turn(board)
     input = gets
     index = input_to_index(input)
     if valid_move?(board, index)
-      move(board, index, char)
+      move(board, index, "X")
     else
       turn(board)
     end
